337f4afc4a78e0876a8b3bcff62eba5cf8a26d54
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-10-20 19:12:25 +0530

    Adding Character Folder

---
+++ Character/CharacterIsLowercase.java
@@ -0,0 +1,12 @@
+public class CharacterIsLowercase {
+    public static void main(String[] args) {
+         char character = 's';
+
+        if (character >= 'a' && character <= 'z') {
+            System.out.println("LowerCase");
+        }
+        else{
+            System.out.println("Not a LowerCase");
+        }
+    }
+}
---
+++ Character/CharacterIsUppercase.java
@@ -0,0 +1,12 @@
+public class CharacterIsUppercase {
+    public static void main(String[] args) {
+        char character = 'D';
+
+        if (character >= 'A' && character <= 'Z') {
+            System.out.println("UpperCase");
+        }
+        else{
+            System.out.println("Not a UpperCase");
+        }
+    }
+}
