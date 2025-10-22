88d47df1f7e9e9e99b21d9e0b028b01169238a0f
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-10-22 14:16:31 +0530

    Adding more arrays program with method

---
+++ Arrays/Notes/PrintAllElementsOfArray.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class PrintAllElementsOfArray {
+    public static void main(String[] args) {
+        int[] arr = { 21, 43, 56, 76, 89, 9, 44 };
+        // printAllElementsOfArray(arr);
+        // int[] result = returnArray(arr);
+        // System.out.println(Arrays.toString(result));
+        // System.out.println(maxElement(arr));
+        // System.out.println(minElement(arr)); 
+        // System.out.println(sumOfElements(arr));
+        int[] result = countEvenOrOdd(arr);
+        System.out.println("Even Count: " + result[0] + ", Odd Count: " + result[1]);
+    }
+
+    static void printAllElementsOfArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static int[] returnArray(int[] arr) {
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return newArr;
+    }
+
+    static int maxElement(int[] arr) {
+        int maxNumber = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxNumber) {
+                maxNumber = arr[i];
+            }
+        }
+        return maxNumber;
+    }
+
+    static int minElement(int[] arr) {
+        int minNumber = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minNumber) {
+                minNumber = arr[i];
+            }
+        }
+        return minNumber;
+    }
+
+    static int sumOfElements(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    static int[] countEvenOrOdd(int[] arr){
+        int countEven = 0;
+        int countOdd = 0;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i] %2==0) {
+                countEven++;
+            }
+            else{
+                countOdd++;
+            }
+        }
+        return new int[]{countEven, countOdd};
+    }
+}
