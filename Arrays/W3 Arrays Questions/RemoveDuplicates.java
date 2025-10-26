eb1629757732d81fecbe9f826a2dc43ee0a93f0b
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-10-26 22:40:01 +0530

    Adding RemoveDuplicates program

--- Arrays/W3 Arrays Questions/RemoveDuplicates.java
+++ Arrays/W3 Arrays Questions/RemoveDuplicates.java
@@ -3,30 +3,27 @@ import java.util.Arrays;
 public class RemoveDuplicates {
     public static void main(String[] args) {
         int[] arr = { 10, 22, 10, 11, 21, 22 };
-        int[] temp = new int[arr.length];
+        int[] tempArr = new int[arr.length];
         int newLength = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i=0;i<arr.length;i++){
             boolean isDuplicate = false;
-            // Check if arr[i] is already present in temp
-            for (int j = 0; j < newLength; j++) {
-                if (arr[i] == temp[j]) {
+            for(int j=0;j<newLength;j++){
+                if (arr[i] == tempArr[j]) {
                     isDuplicate = true;
                     break;
                 }
             }
-            // If not duplicate, add it to temp
             if (!isDuplicate) {
-                temp[newLength++] = arr[i];
+                tempArr[newLength++] = arr[i];
             }
         }
 
-        // Create final array with correct length
-        int[] result = new int[newLength];
-        for (int i = 0; i < newLength; i++) {
-            result[i] = temp[i];
+        int[] resultArr = new int[newLength];
+        for(int i=0;i<newLength; i++){
+            resultArr[i] = tempArr[i];
         }
 
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(resultArr));
     }
 }
