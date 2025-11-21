3a212d46f816627c6448daf4456f93c9a484de08
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-11-21 22:40:02 +0530

    cleaner code  patter 7 program

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,13 +1,13 @@
 public class Pattern7 {
     public static void main(String[] args) {
         int num = 5;
-        for(int row = 1;row<=num;row++){
+        for (int row = 1; row <= num; row++) {
 
-            for(int space=2;space<=row;space++){
+            for (int space = 1; space < row; space++) {
                 System.out.print(" ");
             }
 
-            for(int col=row;col<=num;col++){
+            for (int col = row; col <= num; col++) {
                 System.out.print("*");
             }
             System.out.println();
