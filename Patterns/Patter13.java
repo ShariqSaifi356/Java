54cd83c26c592a93f77ad41bb247f8e7d0d07938
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-11-27 23:12:50 +0530

    correcting pattern 13

--- Patterns/Patter13.java
+++ Patterns/Patter13.java
@@ -5,10 +5,20 @@ public class Patter13 {
         for (int row = 1; row <= num; row++) {
 
             for (int space = num; space > row; space--) {
-                System.out.print(" ");
+                System.out.print("_");
             }
+
             for (int col = 1; col <= 2 * row - 1; col++) {
-                System.out.print("*");
+                // First and last line.
+                if (row == 1 || row == num) {
+                    System.out.print("*");
+                } else {
+                    if (col == 1 || col == 2 * row - 1) {
+                        System.out.print("*");
+                    }else{
+                        System.out.print("_");
+                    }
+                }
             }
             System.out.println();
         }
