9746dca92b0240c41e50e0ad323d5276b65877d5
Mohammad Shariq Saifi <92665673+ShariqSaifi356@users.noreply.github.com>
2025-11-30 23:40:11 +0530

    correcting program 16

--- Patterns/Pattern16.java
+++ Patterns/Pattern16.java
@@ -1,12 +1,17 @@
 public class Pattern16 {
     public static void main(String[] args) {
         int num = 5;
-        for (int row = 1; row <= num; row++) {
+        for (int row = 0; row < num; row++) {
             for (int space = 1; space <= num - row; space++) {
                 System.out.print(" ");
             }
-            for (int col = 1; col <= row; col++) {
-                System.out.print("* ");
+
+            int value = 1;
+
+            for (int col = 0; col <= row; col++) {
+                
+                System.out.print(value + " ");
+                 value = value * (row - col) / (col + 1);
             }
             System.out.println();
         }
